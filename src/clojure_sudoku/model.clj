66b80943fec887cf7b77aa036d66651187b0e5bd
tsumo <tsumo@users.noreply.github.com>
2019-05-19 13:00:13 +0300

    starting to implement Peter Norvig solver model
    because it's clearly superior to my attempts

--- src/clojure_sudoku/model.clj
+++ src/clojure_sudoku/model.clj
@@ -1,132 +1,124 @@
 (ns clojure-sudoku.model)
 
+; Sudoku puzzle is a grid of 81 squares.
+; Collection of nine squares (row, col or box) is a unit.
+; Squares that share a unit a called peers.
+; Each square has 3 units and 20 peers.
 
-(defn prepare-board
-  [grid]
-  (mapv (fn [row]
-          (mapv #(if (nil? %)
-                   (vector nil false)
-                   (vector % true))
-                row))
-        grid))
+; A1 A2 A3 │ A4 A5 A6 │ A7 A8 A9
+; B1 B2 B3 │ B4 B5 B6 │ B7 B8 B9
+; C1 C2 C3 │ C4 C5 C6 │ C7 C8 C9
+; ─────────┼──────────┼──────────
+; D1 D2 D3 │ D4 D5 D6 │ D7 D8 D9
+; E1 E2 E3 │ E4 E5 E6 │ E7 E8 E9
+; F1 F2 F3 │ F4 F5 F6 │ F7 F8 F9
+; ─────────┼──────────┼──────────
+; G1 G2 G3 │ G4 G5 G6 │ G7 G8 G9
+; H1 H2 H3 │ H4 H5 H6 │ H7 H8 H9
+; I1 I2 I3 │ I4 I5 I6 │ I7 I8 I9
 
+(defn cross
+  "Cross-product of two seqs"
+  [s1 s2]
+  (for [x s1
+        y s2]
+    (str x y)))
+
+(def rows [\A \B \C \D \E \F \G \H \I])
+
+(def cols [\1 \2 \3 \4 \5 \6 \7 \8 \9])
+
+(def squares
+  "Every square on the grid"
+  (cross rows cols))
+
+(def unitlist
+  "Every row, col and box of the grid"
+  (concat (map #(cross rows [%]) cols)
+          (map #(cross [%] cols) rows)
+          (map #(apply cross %)
+               (for [r (partition 3 rows)
+                     c (partition 3 cols)]
+                 [r c]))))
+
+(def units
+  "Map of units of the squares"
+  (into {} (map (fn [square]
+                  (vector square
+                          (filter #(some #{square} %)
+                                  unitlist)))
+                squares)))
+
+(def peers
+  "Map of peers of the squares"
+  (into {} (map (fn [square]
+                  (vector square
+                          (disj
+                            (set (apply concat
+                                        (units square)))
+                            square)))
+                squares)))
+
+; Textual representation of the puzzle is a grid
 
 (def starting-grid
-  (prepare-board
-    [[5   3   nil nil 7   nil nil nil nil]
-     [6   nil nil 1   9   5   nil nil nil]
-     [nil 9   8   nil nil nil nil 6   nil]
-     [8   nil nil nil 6   nil nil nil 3]
-     [4   nil nil 8   nil 3   nil nil 1]
-     [7   nil nil nil 2   nil nil nil 6]
-     [nil 6   nil nil nil nil 2   8   nil]
-     [nil nil nil 4   1   9   nil nil 5]
-     [nil nil nil nil 8   nil nil 7   9]]))
-
+  (str "5 3 . │ . 7 . │ . . ."
+       "6 . . │ 1 9 5 │ . . ."
+       ". 9 8 │ . . . │ . 6 ."
+       "──────┼───────┼──────"
+       "8 . . │ . 6 . │ . . 3"
+       "4 . . │ 8 . 3 │ . . 1"
+       "7 . . │ . 2 . │ . . 6"
+       "──────┼───────┼──────"
+       ". 6 . │ . . . │ 2 8 ."
+       ". . . │ 4 1 9 │ . . 5"
+       ". . . │ . 8 . │ . 7 9"))
 
 (def solved-grid
-  (prepare-board
-    [[5 3 4 6 7 8 9 1 2]
-     [6 7 2 1 9 5 3 4 8]
-     [1 9 8 3 4 2 5 6 7]
-     [8 5 9 7 6 1 4 2 3]
-     [4 2 6 8 5 3 7 9 1]
-     [7 1 3 9 2 4 8 5 6]
-     [9 6 1 5 3 7 2 8 4]
-     [2 8 7 4 1 9 6 3 5]
-     [3 4 5 2 8 6 1 7 9]]))
+  (str "5 3 4 │ 6 7 8 │ 9 1 2"
+       "6 7 2 │ 1 9 5 │ 3 4 8"
+       "1 9 8 │ 3 4 2 │ 5 6 7"
+       "──────┼───────┼──────"
+       "8 5 9 │ 7 6 1 │ 4 2 3"
+       "4 2 6 │ 8 5 3 │ 7 9 1"
+       "7 1 3 │ 9 2 4 │ 8 5 6"
+       "──────┼───────┼──────"
+       "9 6 1 │ 5 3 7 │ 2 8 4"
+       "2 8 7 │ 4 1 9 │ 6 3 5"
+       "3 4 5 │ 2 8 6 │ 1 7 9"))
 
-
-(defn gen-grid
-  "Generates sudoku grid"
-  []
-  (repeat 9 (repeat 9 (vector nil false))))
-
-
-(defn get-row
-  "Gets single grid row"
-  [n grid]
-  (nth grid n))
-
-
-(defn get-col
-  "Gets single grid column"
-  [n grid]
-  (mapv #(nth % n) grid))
-
-
-(defn get-box
-  "Gets single grid box"
-  [n grid]
-  (apply concat
-         (mapv #(take 3 (nthnext % (* 3 (mod n 3))))
-               (take 3 (nthnext grid (* 3 (quot n 3)))))))
-
-
-(defn get-all-groups
-  "Gets every row, column and box of the grid"
+(defn parse-grid
   [grid]
-  (concat (concat (map #(get-row % grid) (range 9)))
-          (concat (map #(get-col % grid) (range 9)))
-          (concat (map #(get-box % grid) (range 9)))))
+  "Convert grid to a mapping of squares to collection of possible values.
+  Single value is given to the squares which are determined unambiguosly."
+  (->> grid
+       (filter (set (conj cols \.)))
+       (map #(if (= % \.) (range 1 10) (Character/digit % 10)))
+       (zipmap squares)))
 
 
-(defn get-cell-val
-  "Numerical value of the cell"
-  [cell]
-  (first cell))
+; Printing
 
-
-(defn cell-solved?
-  "Is this cell value determined unambiguously?"
-  [cell]
-  (second cell))
-
-
-(defn group-solved?
-  "Is this group solved?"
-  [group]
-  (every? identity
-          (map (fn [n] (some #(= n (get-cell-val %)) group))
-               (range 1 10))))
-
-
-(defn grid-solved?
-  "Is this grid solved?"
-  [grid]
-  (every? identity
-          (map group-solved? (get-all-groups grid))))
-
-
-(defn set-cell
-  "Updates cell contents using row and col numbers"
-  [[row col] [value final] grid]
-  (assoc-in grid [row col] [value final]))
-
-
-;;; Printing
-
-
-(defn get-cell-string
-  "Printable representation of a single cell"
-  [[value _]]
-  (if (= value nil) \. value))
-
-
-(defn get-row-string
-  "Printable representation of a single row"
-  [row]
-  (clojure.string/join
-    " │ "
-    (map #(clojure.string/join \space %)
-         (partition 3 (map get-cell-string row)))))
-
-
-(defn get-grid-string
-  "Printable representation of a whole grid"
-  [grid]
-  (clojure.string/join
-    "\n──────┼───────┼──────\n"
-    (map #(clojure.string/join \newline %)
-         (partition 3 (map get-row-string grid)))))
+; (defn get-cell-string
+;   "Printable representation of a single cell"
+;   [[value _]]
+;   (if (= value 0) \. value))
+;
+;
+; (defn get-row-string
+;   "Printable representation of a single row"
+;   [row]
+;   (clojure.string/join
+;     " │ "
+;     (map #(clojure.string/join \space %)
+;          (partition 3 (map get-cell-string row)))))
+;
+;
+; (defn get-grid-string
+;   "Printable representation of a whole grid"
+;   [grid]
+;   (clojure.string/join
+;     "\n──────┼───────┼──────\n"
+;     (map #(clojure.string/join \newline %)
+;          (partition 3 (map get-row-string grid)))))
 
---
+++ test/clojure_sudoku/model_test.clj
@@ -0,0 +1,26 @@
+(ns clojure-sudoku.model-test
+  (:require [clojure.test :refer :all]
+            [clojure-sudoku.model :refer :all]))
+
+(deftest model-test
+
+  (testing "Testing model"
+
+    (is (= (count squares) 81))
+
+    (is (= (count unitlist) 27))
+
+    (is (every? #(= (count (units %)) 3) squares))
+
+    (is (every? #(= (count (peers %)) 20) squares))
+
+    (is (= (units "C2")
+           [["A2" "B2" "C2" "D2" "E2" "F2" "G2" "H2" "I2"]
+            ["C1" "C2" "C3" "C4" "C5" "C6" "C7" "C8" "C9"]
+            ["A1" "A2" "A3" "B1" "B2" "B3" "C1" "C2" "C3"]]))
+
+    (is (= (peers "C2")
+           (set ["A2" "B2" "D2" "E2" "F2" "G2" "H2" "I2"
+                 "C1" "C3" "C4" "C5" "C6" "C7" "C8" "C9"
+                 "A1" "A3" "B1" "B3"])))))
+
